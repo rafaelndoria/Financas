23e9828db9c18a40fff696d4554c16577ca2f0dc
Rafael Nascimento <rafaelnd2001@gmail.com>
2023-12-20 00:41:37 -0300

    Adicionado interface para conexao com o banco de dados

---
+++ Financas/Data/DbConnectionProvider.cs
@@ -0,0 +1,58 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using Financas.Data;
+
+public class DbConnectionProvider : IDbConnectionProvider
+{
+    private readonly IDbConnection _connection;
+    private IDbTransaction _transaction;
+
+    public DbConnectionProvider(string connectionString)
+    {
+        _connection = new SqlConnection(connectionString);
+        _connection.Open();
+    }
+
+    public IDbConnection Connection => _connection;
+
+    public IDbTransaction BeginTransaction()
+    {
+        if (_transaction != null)
+        {
+            throw new InvalidOperationException("Já existe uma transação em andamento.");
+        }
+
+        _transaction = _connection.BeginTransaction();
+        return _transaction;
+    }
+
+    public void Commit()
+    {
+        if (_transaction == null)
+        {
+            throw new InvalidOperationException("Nenhuma transação em andamento para confirmar.");
+        }
+
+        _transaction.Commit();
+        _transaction = null;
+    }
+
+    public void Dispose()
+    {
+        if (_connection.State != ConnectionState.Closed)
+        {
+            _connection.Close();
+        }
+    }
+
+    public void Rollback()
+    {
+        if (_transaction == null)
+        {
+            throw new InvalidOperationException("Nenhuma transação em andamento para reverter.");
+        }
+
+        _transaction.Rollback();
+        _transaction = null;
+    }
+}
---
+++ Financas/Data/IDbConnectionProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace Financas.Data
+{
+    public interface IDbConnectionProvider : IDisposable
+    {
+        IDbConnection Connection { get; }
+        IDbTransaction BeginTransaction();
+        void Commit();
+        void Rollback();
+    }
+}
